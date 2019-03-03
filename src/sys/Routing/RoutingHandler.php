c540640e256f8fb27ae1a7b07458fca68df5e23d
AkronimBlack <bojanmadzaracka@gmail.com>
2019-03-03 08:57:37 +0100

    works? this will fail... somewhere... somehow

--- src/sys/Routing/RoutingHandler.php
+++ src/sys/Routing/RoutingHandler.php
@@ -9,9 +9,35 @@ class RoutingHandler
 {
     /**
      * @param Request $request
+     * @param array $routs
      */
     public function execute(Request $request, array $routs)
     {
-        var_dump($routs);
+        var_dump($this->resolveRoute($request, $routs));
+    }
+
+    /**
+     * @param Request $request
+     * @param array $routes
+     *
+     * @return array
+     */
+    private function resolveRoute(Request $request, array $routes): array
+    {
+        foreach ($routes as $route => $call)
+        {
+            if($request->getUri() === $route)
+            {
+                list($controller , $method) = explode('/', $call);
+                return array(
+                    'controller' => $controller,
+                    'method' => $method
+                );
+            }
+        }
+        return array(
+            'controller' => 'IndexController',
+            'method' => 'index'
+        );
     }
 }
