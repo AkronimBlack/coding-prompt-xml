c0873868d1f5ad3308c6ad2dc20f1f633ffc4f33
AkronimBlack <bojanmadzaracka@gmail.com>
2019-03-03 08:45:08 +0100

    added params to request

--- src/sys/Entity/Request.php
+++ src/sys/Entity/Request.php
@@ -6,9 +6,42 @@ namespace src\sys\Entity;
 class Request
 {
 
+    private $uri;
+    private $query;
+    private $method;
+
     public function __construct(
+        $uri,
+        $method,
+        $query
     ) {
-        echo 'test';
+        $this->uri    = $uri;
+        $this->query  = urldecode($query);
+        $this->method = $method;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMethod()
+    {
+        return $this->method;
     }
 
 
