e460f40bb33f216d97f41964448ecf1223370398
AkronimBlack <bojanmadzaracka@gmail.com>
2019-03-03 08:37:42 +0100

    index.php autoloader and bootstrap

---
+++ src/Bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace src;
+
+/**
+ * Class Bootstrap
+ * @package src
+ */
+class Bootstrap
+{
+    /**
+     * Bootstrap constructor.
+     */
+    public function __construct()
+    {
+        echo 'test';
+    }
+
+}
